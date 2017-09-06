9e66840f42c35ec850547e9d70390410d42b34f5
Wenfei Cao <admin@cwf123.com>
2017-09-06 11:36:10 -0700

    Put debug character in the scene

--- Commute Rush/Assets/_Scripts/PlayerControl.cs
+++ Commute Rush/Assets/_Scripts/PlayerControl.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerControl : MonoBehaviour {
+    public float jumpForce = 100;
 
 	// Use this for initialization
 	void Start () {
@@ -11,6 +12,9 @@ public class PlayerControl : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (Input.GetKey(KeyCode.Space))
+        {
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpForce));
+        }
 	}
 }
