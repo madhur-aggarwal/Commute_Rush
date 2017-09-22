2b0ddd1e4cacd2caa2ef627601fad39fb5bf4e72
Wenfei Cao <admin@cwf123.com>
2017-09-22 15:46:46 -0700

    more color sprite, emptied player control script

--- Commute Rush/Assets/_Scripts/PlayerControl.cs
+++ Commute Rush/Assets/_Scripts/PlayerControl.cs
@@ -6,9 +6,7 @@ public class PlayerControl : MonoBehaviour {
     private GameManager gm;
 
     public float jumpForce = 300;
-    public int maxJump = 2;
 
-    private int currentJump = 0;
 
 	// Use this for initialization
 	void Start () {
@@ -21,27 +19,19 @@ public class PlayerControl : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKey(KeyCode.Space))
+		if (Input.GetKey(KeyCode.LeftArrow))
         {
-            if (currentJump <= maxJump)
-            {
-                GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpForce));
-                currentJump++;
-            }
             
+            
+        }else if (Input.GetKey(KeyCode.RightArrow))
+        {
+
         }
 	}
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "Obstacle")
-        {
-            currentJump = 0;
-        }
-        else if (collision.gameObject.tag == "DeathZone")
-        {
-            gm.GameOver();
-        }
+        
              
     }
 
